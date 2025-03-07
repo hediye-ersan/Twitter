83c930a801d6bb08e9bcf06642a0532575483fa6
Hediye Ersan <hediyesnl9@gmail.com>
2025-03-07 17:01:24 +0300

    LikRepository revize eder

--- src/main/java/com/workintech/Twitter/repository/LikeRepository.java
+++ src/main/java/com/workintech/Twitter/repository/LikeRepository.java
@@ -4,7 +4,9 @@ import com.workintech.Twitter.entity.Like;
 import com.workintech.Twitter.entity.Tweet;
 import com.workintech.Twitter.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -12,14 +14,16 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 
 
     //Kullanıcının tweeti beğenip beğenmediğini kontrol etmesini sağlar
-    @Query("SELECT l FROM Like l WHERE l.user.id = userId AND l.tweet.id = tweetId")
+    @Query("SELECT l FROM Like l WHERE l.user.id = :userId AND l.tweet.id =:tweetId")
     Optional<Like> findByUserAndTweet(User user, Tweet tweet);
 
     //Tweetin kaç beğeni aldığını sayar
-    @Query("SELECT COUNT(l) FROM Like l WHERE l.tweet.id = tweetId")
+    @Query("SELECT COUNT(l) FROM Like l WHERE l.tweet.id =:tweetId")
     Long countByTweetId(Long tweetId);
 
-    //Kullanıcının tweetdeki beğenisini silmesini sağlar
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Like l WHERE l.user = :user AND l.tweet = :tweet")
     void deleteByUserAndTweet(User user, Tweet tweet);
 
 }
