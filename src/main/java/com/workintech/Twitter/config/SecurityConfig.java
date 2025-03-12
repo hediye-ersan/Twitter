cbe18fdce66d2691ce68f0e81e671da411d40f48
hediye-ersan <hediyesnl9@gmail.com>
2025-03-13 00:47:11 +0300

    Cors hatasını çözer

--- src/main/java/com/workintech/Twitter/config/SecurityConfig.java
+++ src/main/java/com/workintech/Twitter/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/user/register", "/user/login").permitAll()
                         .anyRequest().authenticated())
